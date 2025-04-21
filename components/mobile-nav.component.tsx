c047d7adb5f3f08c8bb91e470a5ba43106d22fe7
nimiakinleye <toluwanimiakinleye01@gmail.com>
2025-04-21 13:31:14 +0100

    feat: mobile navigation acive state

--- components/mobile-nav.component.tsx
+++ components/mobile-nav.component.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import SVG, { IconType } from "./svg.component";
 import Link from "next/link";
 import routes from "@/helpers/routes";
+import { useRouter } from "next/router";
 
 const MOBILE_NAV_LINKS: { icon: IconType; url: string }[] = [
   { icon: "home", url: routes.home() },
@@ -11,6 +12,12 @@ const MOBILE_NAV_LINKS: { icon: IconType; url: string }[] = [
 ];
 
 export default function MobileNav() {
+  const { pathname, asPath } = useRouter();
+
+  const isNavActive = (url: string) => {
+    console.log(url, pathname, asPath);
+    return url === routes.home() ? url === pathname : asPath.startsWith(url);
+  };
   return (
     <nav className="glass w-full fixed bottom-0 pt-[9px] pb-[20px] px-[30px] rounded-[20px] flex md:hidden justify-around items-end">
       {MOBILE_NAV_LINKS.map((el, key) => (
@@ -19,11 +26,15 @@ export default function MobileNav() {
           key={key}
           className="flex flex-col gap-3 items-center"
         >
-          {key === 0 && (
+          {isNavActive(el.url) && !["chart", "bag"].includes(el.icon) && (
             <div className="size-[8px] rounded-full bg-white"></div>
           )}
           <SVG
-            color={key === 0 ? "#ffffff" : "#BAC2CC"}
+            color={
+              isNavActive(el.url) && !["chart", "bag"].includes(el.icon)
+                ? "#ffffff"
+                : "#BAC2CC"
+            }
             width={24}
             height={24}
             iconName={el.icon}
