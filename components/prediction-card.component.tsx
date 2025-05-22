f4cd6818367131b2254fc0b26921e9b7992c750a
SkyC0der <emmanuelezenwigbo@gmail.com>
2025-05-22 02:41:11 +0100

    fix conflict

--- components/prediction-card.component.tsx
+++ components/prediction-card.component.tsx
@@ -229,7 +229,7 @@ export default function PredictionCard({
     return (
       <div className="flex-1 glass h-[300px] flex flex-col justify-between gap-[13px] rounded-[20px] px-[19px] py-[8.5px]">
         {
-          timeLeft == null ? (
+          isLocked ? (
             <div className="flex flex-col items-center justify-center h-full">
               <DotLoader
                 color="#06C729"
