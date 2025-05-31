683db4b7c656cf179afe033cea7c6752a9d8bd77
SkyC0der <emmanuelezenwigbo@gmail.com>
2025-05-31 11:32:45 +0100

    IP address Block

--- lib/config.ts
+++ lib/config.ts
@@ -1,2 +1,2 @@
-export const API_URL = "https://sol-prediction-backend.onrender.com"
-export const PROGRAM_ID = "AKui3UEpyUEhtnqsDChTL76DFncYx6rRqp6CSShnUm9r"
+export const API_URL = "https://sol-prediction-backend-6e3r.onrender.com"
+export const PROGRAM_ID = "BLrGsehZZ8kgkUXrkken588MeEDDZXdxvcX7hR5HEgzA"
