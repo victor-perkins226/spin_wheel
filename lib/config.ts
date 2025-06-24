90d5745267a0364386d0f7b14b2a616ef701bb58
cryptobear <sacred.danny.jin@gmail.com>
2025-06-24 13:17:00 -0700

    update idl

--- lib/config.ts
+++ lib/config.ts
@@ -1,3 +1,3 @@
 export const API_URL = "https://sol-prediction-backend-6e3r.onrender.com"
-export const PROGRAM_ID = "51LsCdruBZE3bi5KeNwpjVqDcpwGKXppzZQGrD3xqJZ9"
+export const PROGRAM_ID = "FKxohzkYZR8wRct6cZGiSdrrhDKtby3m27smuui8xuXK"
 export const RPC_URL = "https://lb.drpc.org/ogrpc?network=solana-devnet&dkey=AqnRwY5nD0C_uEv_hPfBwlLj0fFzMcQR8JKdzoXPVSjK"
