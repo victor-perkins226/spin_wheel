772da636743ba32bb489a9242b91f229a198042e
cryptobear <sacred.danny.jin@gmail.com>
2025-07-03 14:36:07 -0700

    update rpc

--- lib/config.ts
+++ lib/config.ts
@@ -1,3 +1,3 @@
 export const API_URL = "https://sol-prediction-backend-6e3r.onrender.com"
 export const PROGRAM_ID = "GHxMXAX2TPKSYgMTM92NfsJEm2NGpt9j5fWGWypTF34R"
-export const RPC_URL = "https://lb.drpc.org/ogrpc?network=solana-devnet&dkey=AqnRwY5nD0C_uEv_hPfBwlLj0fFzMcQR8JKdzoXPVSjK"
+export const RPC_URL = "https://aged-shy-reel.solana-devnet.quiknode.pro/07d068bb327007c04b51ad94c8290badbb1c2765/"
