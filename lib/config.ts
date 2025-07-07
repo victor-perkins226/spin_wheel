66dafd440d7e00450604b9c5f970090ef18ab908
cryptobear <sacred.danny.jin@gmail.com>
2025-07-06 18:58:54 -0700

    update

--- lib/config.ts
+++ lib/config.ts
@@ -1,3 +1,3 @@
 export const API_URL = "https://sol-prediction-backend-6e3r.onrender.com"
-export const PROGRAM_ID = "GHxMXAX2TPKSYgMTM92NfsJEm2NGpt9j5fWGWypTF34R"
+export const PROGRAM_ID = "2Lcsj4s1MM4B4mdAc9ii46JNB8BaQZwq87SnuT7YP4Qc"
 export const RPC_URL = "https://aged-shy-reel.solana-devnet.quiknode.pro/07d068bb327007c04b51ad94c8290badbb1c2765/"
