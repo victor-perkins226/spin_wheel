114a7d15961aef28d7e1c2a994ef753821d7f3ac
SkyC0der <emmanuelezenwigbo@gmail.com>
2025-07-17 11:03:18 +0100

    Chore: Disabled LogRocket on Leaderboard Page

--- lib/logrocket.ts
+++ lib/logrocket.ts
@@ -1,7 +1,20 @@
 import LogRocket from 'logrocket';
 
 export const initLogRocket = () => {
-  if (typeof window !== 'undefined' && process.env.NODE_ENV === 'production') {
-    LogRocket.init('udpyby/fortuva-sol-prediction');
+  if (typeof window === 'undefined' || process.env.NODE_ENV !== 'production') {
+    return;
   }
+
+  const path = window.location.pathname;
+
+  const options = path === '/leaderboard'
+    ? {
+        network: { isEnabled: false },
+        console: { isEnabled: false },
+        dom:     { isEnabled: false },
+      }
+    : undefined;
+
+  LogRocket.init('udpyby/fortuva-sol-prediction', options);
 };
+
